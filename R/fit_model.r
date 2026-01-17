5d51c39b31b6b7f961d7eb3ffadb079bbf7215a2
clarabrigitta <156320564+clarabrigitta@users.noreply.github.com>
2026-01-17 15:40:01 +0000

    update model fitting script

--- R/fit_model.r
+++ R/fit_model.r
@@ -3,7 +3,11 @@
 # Selecting data for Rhinovirus Jan 2020 - Dec 2021 ----
 data <- read_csv("inst/data/respiratory_scot_20250917.csv") %>% 
   mutate(WeekBeginning = as.Date(as.character(WeekBeginning), format = "%Y%m%d")) %>% 
-  filter(Pathogen == "Respiratory syncytial virus",
+  filter(!Pathogen %in% c("Influenza - Type A (not subtyped)",
+                          "Influenza - Type A or B",
+                          "Influenza - Type A(H1N1)pdm09" ,
+                          "Influenza - Type A(H3)",
+                          "Mycoplasma pneumoniae" ),
          WeekBeginning >= as.Date("2020-03-23"),
          WeekBeginning <= as.Date("2022-03-02"))
 
@@ -60,7 +64,7 @@ correlationPlot(out)
 
 # run with samples ----
 
-posterior <- getSample(out)
+posterior <- getSample(out, start = 2)
 
 traj <- lapply(1:nrow(posterior),
                function(r){
@@ -82,18 +86,15 @@ traj <- lapply(1:nrow(posterior),
 
 traj_all <- do.call(cbind, traj) %>% 
   as.data.frame() %>%
-  setNames(as.character(1:405)) %>% 
   bind_cols(data[, c(1, 4)]) %>% 
   pivot_longer(cols = c(1:length(traj)), names_to = "iter", values_to = "count")
 
 traj_hdi <- test <- do.call(cbind, traj) %>% 
   as.data.frame() %>%
-  setNames(as.character(1:405)) %>% 
   t() %>% 
   hdi() %>%
   rbind(mean = colMeans(do.call(cbind, traj) %>% 
                           as.data.frame() %>%
-                          setNames(as.character(1:405)) %>% 
                           t())) %>% 
   t() %>% 
   as.data.frame() %>% 
@@ -110,9 +111,13 @@ ggplot() +
   theme_classic()
 
 ggplot() +
-  # geom_line(data = traj_hdi, aes(x = date, y = mean), color = "blue") +
+  # geom_line(data = traj_hdi, aes(x = date, y = mean), color = "blue", size = 1) +
   # geom_ribbon(data = traj_hdi, aes(x = date, ymin = lower, ymax = upper), alpha = 0.2) +
-  geom_line(data = data, aes(x = WeekBeginning, y = NumberCasesPerWeek), alpha = 0.7, color = "black") +
-  theme(legend.position = "none") +
+  geom_line(data = combined, aes(x = date, y = mean_contacts*25, colour = part_age)) +
+  geom_line(data = data, aes(x = WeekBeginning, y = NumberCasesPerWeek), alpha = 0.7, color = "red", size = 1) +
+  scale_y_continuous(name = "Count", 
+                     sec.axis = sec_axis(trans = ~./25, name = "Number of contacts")) +
+  scale_color_viridis(discrete = T, option = "D") +
   labs(x = "Months", y = "Count") + 
-  theme_classic()
+  theme_classic() +
+  facet_wrap(~Pathogen)
