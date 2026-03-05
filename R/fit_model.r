231b2f6c61a2508c287ca961dc3ad32626d2a326
clarabrigitta <156320564+clarabrigitta@users.noreply.github.com>
2026-03-05 18:20:19 +0000

    expand fitting for multiple viruses and partial automation

--- R/fit_model.r
+++ R/fit_model.r
@@ -1,192 +1,177 @@
-# fit SEIRS model to data ----
+# model fitting for all combinations ----
 
-# Selecting data for RSV ----
-data <- read_csv("inst/data/respiratory_scot_20250917.csv") %>% 
-  mutate(WeekBeginning = as.Date(as.character(WeekBeginning), format = "%Y%m%d")) %>% 
+## helper data and functions ----
+### pathogen name reference: name in combination data frame vs name in Scottish data
+pathogen_map <- c(
+  fluA = "Influenza - Type A (any subtype)",
+  fluB = "Influenza - Type B",
+  RSV = "Respiratory syncytial virus",
+  hCOV = "Seasonal coronavirus (Non-SARS-CoV-2)",
+  AdV = "Adenovirus",
+  RV = "Rhinovirus",
+  hMPV = "Human metapneumovirus",
+  PIV = "Parainfluenza virus"
+)
+
+### load full dataset
+data <- read_csv("inst/data/respiratory_scot_20250917.csv") %>%
+  mutate(WeekBeginning = as.Date(as.character(WeekBeginning), format = "%Y%m%d")) %>%
   filter(!Pathogen %in% c("Influenza - Type A (not subtyped)",
                           "Influenza - Type A or B",
-                          "Influenza - Type A(H1N1)pdm09" ,
+                          "Influenza - Type A(H1N1)pdm09",
                           "Influenza - Type A(H3)",
-                          "Mycoplasma pneumoniae" ),
+                          "Mycoplasma pneumoniae"),
          WeekBeginning >= as.Date("2020-03-23"),
-         WeekBeginning <= as.Date("2022-03-02")) %>% 
-  filter(Pathogen == "Respiratory syncytial virus") %>% 
-  head(101)
+         WeekBeginning <= as.Date("2022-03-02"))
 
-## function to run model; things to still fit: seeding timing 
-# run_model <- function(I0, R0, omega, p_inf){
-#   N <- 5500000
-#   I0 <- I0
-#   E0 <- 0
-#   R0 <- R0
-#   S0 <- N - I0 - E0 - R0
-#   y0 <- c(S = S0, E = E0, I = I0, R = R0)
-#   
-#   out <- ode(y = y0, times = times, func = seirs, parms = list(N = N, sigma = sigma, gamma = gamma, omega = omega, p_inf = p_inf)) %>% 
-#     as.data.frame() %>% 
-#     mutate(date = as.Date(as.Date("2020-03-23"):as.Date("2022-03-02")), 
-#            week_beginning = floor_date(date, unit = "week", week_start = 1)) %>% 
-#     group_by(week_beginning) %>% 
-#     summarise(inc = sum(inc))
-#   
-#   return(out)
-# }
+### date constants used
+dates_seq  <- seq.Date(as.Date("2020-03-23"), as.Date("2022-02-26"), by = 1)
+week_start <- floor_date(dates_seq, unit = "week", week_start = 1)
 
-# run_model <- function(mu, sigma){
-#   y0 <- c(S1 = tot1-5-floor(tot1*(1- imm_distribution(1, mu, sigma))), E1 = 0, I1 = 5, R1 = floor(tot1*(1- imm_distribution(1, mu, sigma))),
-#           S2 = tot2-5-floor(tot2*(1- imm_distribution(2, mu, sigma))), E2 = 0, I2 = 5, R2 = floor(tot2*(1- imm_distribution(2, mu, sigma))),
-#           S3 = tot3-5-floor(tot3*(1- imm_distribution(3, mu, sigma))), E3 = 0, I3 = 5, R3 = floor(tot3*(1- imm_distribution(3, mu, sigma))),
-#           S4 = tot4-5-floor(tot4*(1- imm_distribution(4, mu, sigma))), E4 = 0, I4 = 5, R4 = floor(tot4*(1- imm_distribution(4, mu, sigma))),
-#           S5 = tot5-5-floor(tot5*(1- imm_distribution(5, mu, sigma))), E5 = 0, I5 = 5, R5 = floor(tot5*(1- imm_distribution(5, mu, sigma))),
-#           S6 = tot6-5-floor(tot6*(1- imm_distribution(6, mu, sigma))), E6 = 0, I6 = 5, R6 = floor(tot6*(1- imm_distribution(6, mu, sigma))),
-#           S7 = tot7-5-floor(tot7*(1- imm_distribution(7, mu, sigma))), E7 = 0, I7 = 5, R7 = floor(tot7*(1- imm_distribution(7, mu, sigma))),
-#           S8 = tot8-5-floor(tot8*(1- imm_distribution(8, mu, sigma))), E8 = 0, I8 = 5, R8 = floor(tot8*(1- imm_distribution(8, mu, sigma))),
-#           S9 = tot9-5-floor(tot9*(1- imm_distribution(9, mu, sigma))), E9 = 0, I9 = 5, R9 = floor(tot9*(1- imm_distribution(9, mu, sigma))))
-#
-#   out <- ode(y = y0, times = times, func = seirs,
-#              parms = list(sigma = 1 / combinations[[n]]$inc_period, gamma = 1 / combinations[[n]]$inf_period, omega = 1 / combinations[[n]]$imm_period, p_inf = combinations[[n]]$p_inf, mu_b = daily_births, mu_d = hazard_death)) %>%
-#     as.data.frame() %>%
-#     select(matches("inc|time")) %>%
-#     pivot_longer(cols = 2:10) %>%
-#     group_by(time) %>%
-#     summarise(inc = sum(value)) %>%
-#     mutate(date = as.Date(as.Date("2020-03-23"):as.Date("2022-02-26")),
-#            week_beginning = floor_date(date, unit = "week", week_start = 1)) %>%
-#     group_by(week_beginning) %>%
-#     summarise(inc = sum(inc))
-#
-#   return(out)
-# }
+### population totals by age group
+tots <- c(tot1, tot2, tot3, tot4, tot5, tot6, tot7, tot8, tot9)
 
-run_model <- function(mu, sigma){
-  # vectorise initial conditions: compute imm for all 9 groups in one call
-  tots <- c(tot1, tot2, tot3, tot4, tot5, tot6, tot7, tot8, tot9)
-  imm  <- imm_distribution(1:9, mu, sigma)
-  R_init <- floor(tots * (1 - imm))
+## run model fitting ----
+run_model <- function(p_sus, n, imm_days) {
+  R_init <- floor(tots * (1 - p_sus))
   y0_mat <- rbind(S = tots - 5 - R_init, E = rep(0, 9), I = rep(5, 9), R = R_init)
   y0 <- setNames(as.vector(y0_mat), paste0(rep(c("S", "E", "I", "R"), 9), rep(1:9, each = 4)))
-  
-  out_mat <- ode(y = y0, times = times, func = seirs,
-                 parms = list(sigma = 1 / combinations[[n]]$inc_period, gamma = 1 / combinations[[n]]$inf_period,
-                              omega = 1 / combinations[[n]]$imm_period, p_inf = combinations[[n]]$p_inf,
-                              mu_b = daily_births, mu_d = hazard_death))
-  
-  # base R aggregation: avoids pivot_longer / group_by overhead on every likelihood call
-  inc_cols  <- grep("^inc", colnames(out_mat))
-  daily_inc <- rowSums(out_mat[, inc_cols, drop = FALSE])
-  dates_seq <- seq.Date(as.Date("2020-03-23"), as.Date("2022-02-26"), by = 1)
-  week_start <- floor_date(dates_seq, unit = "week", week_start = 1)
+
+  out <- ode(y = y0, times = times, func = seirs,
+             parms = list(sigma = 1 / combinations[[n]]$inc_period, gamma = 1 / combinations[[n]]$inf_period,
+                          omega = 1 / imm_days, p_inf = combinations[[n]]$p_inf,
+                          mu_b = daily_births, mu_d = hazard_death))
+
+  inc_cols  <- grep("^inc", colnames(out))
+  daily_inc <- rowSums(out[, inc_cols, drop = FALSE])
   weekly_inc <- tapply(daily_inc, week_start, sum)
-  
+
   data.frame(week_beginning = as.Date(names(weekly_inc)),
              inc = as.numeric(weekly_inc))
 }
 
-# bayesian model specification ----
-# likelihood <- function(param) {
-#   
-#   initial_I <- param[1]
-#   initial_R <- param[2]
-#   imm_period <- param[3]
-#   prob_inf <- param[4]
-#   detection_rate <- param[5]
-#   
-#   likelihood <- dpois(data$NumberCasesPerWeek,
-#                       run_model(I0 = initial_I, R0 = initial_R, omega = 1/imm_period, p_inf = prob_inf)$inc * detection_rate,
-#                       log = T)
-#   
-#   return(sum(likelihood)) 
-# }
+### bayesian specifications
+settings <- list(iterations = 10000, burnin = 2000,  nrChains = 1)
 
-likelihood <- function(param) {
-  
-  imm_mu <- param[1]
-  imm_sigma <- param[2]
-  detection_rate <- param[3]
-  
-  likelihood <- dpois(data$NumberCasesPerWeek,
-                      run_model(mu = imm_mu, sigma = imm_sigma)$inc * detection_rate,
-                      log = T)
-  
-  return(sum(likelihood)) 
+### fit all combinations
+results <- list()
+
+for (n in seq_along(combinations)) {
+  virus_name <- combinations[[n]]$name
+  pathogen_name <- pathogen_map[[virus_name]]
+  cat("Fitting combination", n, ":", virus_name)
+
+  # filter for specific pathogen data
+  subdata <- data %>%
+    filter(Pathogen == pathogen_name) %>%
+    head(101)
+
+  local({
+    n_local    <- n
+    data_local <- subdata
+    # specify priors based on which virus
+    prior_local <- createUniformPrior(lower = combinations[[n_local]]$lb, upper = combinations[[n_local]]$ub)
+
+    likelihood <- function(param) {
+      sus_dist <- param[1]
+      detection_rate <- param[2]
+      imm_duration <- param[3]
+
+      ll <- dpois(data_local$NumberCasesPerWeek,
+                  run_model(p_sus = sus_dist, n = n_local, imm_days = imm_duration)$inc * detection_rate,
+                  log = TRUE)
+      return(sum(ll))
+    }
+
+    setup <- createBayesianSetup(likelihood = likelihood, prior = prior_local, parallel = FALSE, names = c("sus_dist", "detection_rate", "imm_duration"))
+
+    set.seed(24)
+    out <- runMCMC(bayesianSetup = setup, sampler = "DEzs", settings = settings)
+
+    results[[virus_name]] <<- out
+  })
+
+  cat("  Done:", virus_name, "\n")
 }
 
-prior <- createUniformPrior(lower = c(1, 1, 0), upper = c(10, 20, 1))
+saveRDS(results, file = here("inst", "outdata", "parameters_sus_det_imm_05032026"))
 
-# setup <- createBayesianSetup(likelihood = likelihood, prior = prior)
-# settings <- list(iterations = 5000, nrChains = 1, burnin = 2500, thin = 5)
+### manual diagnostic checks
+summary(results[["RV"]])
+getSample(results[["PIV"]], start = 2)
+plot(results[["RV"]], start = 2)
+correlationPlot(results[["fluA"]])
 
-# parallel = TRUE runs each chain on a separate core (DEzs requires >= 3 chains)
-setup <- createBayesianSetup(likelihood = likelihood, prior = prior, parallel = TRUE)
+## run with posterior samples ----
+results_traj <- list()
 
-# run sampler ----
-settings <- list(iterations = 5000, nrChains = 1, burnin = 2000, thin = 5)
+for (n in seq_along(combinations)) {
+  virus_name <- combinations[[n]]$name
+  pathogen_name <- pathogen_map[[virus_name]]
+  cat("Trajectory", n, ":", virus_name)
+  
+  local({
+    n_local   <- n
+    posterior <- getSample(results[[n_local]], start = 2)
+    
+    traj <- lapply(1:nrow(posterior),
+                   function(r){
+                     run_model(p_sus = posterior[r, 1], n = n_local, imm_days = posterior[r, 3])$inc * posterior[r, 2]
+                   })
+    
+    results_traj[[virus_name]] <<- traj
+  })
+    
+  cat("  Done:", virus_name, "\n")
+}
 
-set.seed(24)
-out <- runMCMC(bayesianSetup = setup, sampler = "DEzs", settings = settings)
-f
-## retrieve results
-getSample(out)
-summary(out)
-plot(out)
-correlationPlot(out)
+saveRDS(results_traj, file = here("inst", "outdata", "traj_sus_det_imm_05032026"))
 
-# run with samples ----
+## process trajectories for plotting ----
+### choose which virus trajectory and data
+n = 8
+virus_name <- combinations[[n]]$name
+pathogen_name <- pathogen_map[[virus_name]]
 
-posterior <- getSample(out, start = 2)
+traj <- results_traj[[virus_name]]
+data <- all_data %>%
+  filter(Pathogen == pathogen_name) %>%
+  head(101)
 
-traj <- lapply(1:nrow(posterior),
-               function(r){
-                 N <- 5500000
-                 I0 <- as.numeric(posterior[r, 1])
-                 E0 <- 0
-                 R0 <- as.numeric(posterior[r, 2])
-                 S0 <- N - I0 - E0 - R0
-                 y0 <- c(S = S0, E = E0, I = I0, R = R0)
-                 
-                 ode(y = y0, times = times, func = seirs, parms = list(N = N, sigma = sigma, gamma = gamma, omega = posterior[r, 3], p_inf = posterior[r, 4])) %>% 
-                   as.data.frame() %>% 
-                   mutate(date = as.Date(as.Date("2020-03-23"):as.Date("2022-03-02")), 
-                          week_beginning = floor_date(date, unit = "week", week_start = 1)) %>% 
-                   group_by(week_beginning) %>% 
-                   summarise(inc = sum(inc) * posterior[r, 5]) %>% 
-                   select(inc)
-               })
-
-traj_all <- do.call(cbind, traj) %>% 
+traj_all <- do.call(cbind, traj) %>%
   as.data.frame() %>%
-  bind_cols(data[, c(1, 4)]) %>% 
+  bind_cols(data[, c(1, 2)]) %>%
   pivot_longer(cols = c(1:length(traj)), names_to = "iter", values_to = "count")
 
-traj_hdi <- test <- do.call(cbind, traj) %>% 
+traj_hdi <- do.call(cbind, traj) %>%
   as.data.frame() %>%
-  t() %>% 
+  t() %>%
   hdi() %>%
-  rbind(mean = colMeans(do.call(cbind, traj) %>% 
+  rbind(mean = colMeans(do.call(cbind, traj) %>%
                           as.data.frame() %>%
-                          t())) %>% 
-  t() %>% 
-  as.data.frame() %>% 
+                          t())) %>%
+  t() %>%
+  as.data.frame() %>%
   mutate(date = unique(data$WeekBeginning))
 
-
-# plot trajectories ----
+## plot trajectories ----
 ggplot() + 
   geom_line(data = traj_all, aes(x = WeekBeginning, y = count, group = iter), alpha = 0.05, color = "darkgrey") +
   geom_line(data = data, aes(x = WeekBeginning, y = NumberCasesPerWeek), alpha = 0.7, color = "black") +
-  ylim(0, 5000) +
+  # ylim(0, 5000) +
   theme(legend.position = "none") +
-  labs(x = "Months", y = "Count") + 
+  labs(x = "Weeks", y = "Count") + 
   theme_classic()
 
 ggplot() +
-  # geom_line(data = traj_hdi, aes(x = date, y = mean), color = "blue", size = 1) +
-  # geom_ribbon(data = traj_hdi, aes(x = date, ymin = lower, ymax = upper), alpha = 0.2) +
-  geom_line(data = combined, aes(x = date, y = mean_contacts*25, colour = part_age)) +
-  geom_line(data = data, aes(x = WeekBeginning, y = NumberCasesPerWeek), alpha = 0.7, color = "red", size = 1) +
-  scale_y_continuous(name = "Count", 
-                     sec.axis = sec_axis(trans = ~./25, name = "Number of contacts")) +
-  scale_color_viridis(discrete = T, option = "D") +
-  labs(x = "Months", y = "Count") + 
+  geom_line(data = traj_hdi, aes(x = date, y = mean), color = "red", linewidth = 1) +
+  geom_ribbon(data = traj_hdi, aes(x = date, ymin = lower, ymax = upper), fill = "red", alpha = 0.2) +
+  geom_line(data = data, aes(x = WeekBeginning, y = NumberCasesPerWeek), alpha = 0.7, color = "black", size = 1) +
+  scale_x_date(date_breaks = "3 month") +
+  labs(x = "Weeks", y = "Count", title = virus_name) +
   theme_classic() +
-  facet_wrap(~Pathogen)
+  theme(axis.text.x = element_text(angle = 45, hjust = 1),
+        axis.text=element_text(size=12),
+        axis.title=element_text(size=14),
+        legend.text=element_text(size=12),
+        legend.title=element_text(size=14))
