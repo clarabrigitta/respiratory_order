5e8e9f148f892b2770ed7957dd879efb51abcceb
clarabrigitta <156320564+clarabrigitta@users.noreply.github.com>
2026-03-11 13:51:28 +0000

    plot rt trajectories and overlay contacts on epidemic trajectories

--- R/plot_summary.r
+++ R/plot_summary.r
@@ -6,8 +6,7 @@ for (virus_name in names(results_traj)) {
   
   traj <- results_traj[[virus_name]]
   subdata <- data %>% 
-    filter(Pathogen == pathogen_name) %>% 
-    head(101)
+    filter(Pathogen == pathogen_name) 
   
   traj_all <- do.call(cbind, traj) %>%  as.data.frame() %>%  t()
   traj_hdi <- traj_all %>% 
@@ -17,19 +16,33 @@ for (virus_name in names(results_traj)) {
     as.data.frame() %>% 
     mutate(date = unique(subdata$WeekBeginning))
   
-  plots[[virus_name]] <- ggplot() +
-    geom_ribbon(data = traj_hdi, aes(x = date, ymin = lower, ymax = upper), fill = "red", alpha = 0.2) +
-    geom_line(data = traj_hdi, aes(x = date, y = mean, color = "Model estimate"), linewidth = 1) +
-    geom_line(data = subdata, aes(x = WeekBeginning, y = NumberCasesPerWeek, color = "Data"), alpha = 0.7, linewidth = 1) +
-    scale_color_manual(name = NULL, values = c("Data" = "black", "Model estimate" = "red")) +
-    scale_x_date(date_breaks = "3 month") +
-    labs(x = "Weeks", y = "Count", title = virus_name) +
-    theme_classic() +
-    theme(axis.text.x = element_text(angle = 45, hjust = 1),
-          axis.text = element_text(size = 10),
-          axis.title = element_text(size = 12),
-          legend.text = element_text(size = 11),
-          plot.title = element_text(size = 12, face = "bold"))
+  local({
+    scale_factor <- max(subdata$NumberCasesPerWeek, na.rm = TRUE) / max(combined$mean_contacts, na.rm = TRUE)
+    subcombined <- combined %>% mutate(contacts_scaled = mean_contacts * scale_factor)
+    contact_breaks <- seq(0, ceiling(max(combined$mean_contacts)), by = 2)
+
+    plots[[virus_name]] <<- ggplot() +
+      geom_ribbon(data = traj_hdi, aes(x = date, ymin = lower, ymax = upper), fill = "red", alpha = 0.2) +
+      geom_line(data = traj_hdi, aes(x = date, y = mean, color = "Model estimate"), linewidth = 1) +
+      geom_line(data = subdata, aes(x = WeekBeginning, y = NumberCasesPerWeek, color = "Data"), alpha = 0.7, linewidth = 1) +
+      geom_line(data = subcombined, aes(x = date, y = contacts_scaled), colour = "blue", lty = 2, linewidth = 1) +
+      scale_color_manual(name = NULL, values = c("Data" = "black", "Model estimate" = "red")) +
+      scale_x_date(date_breaks = "3 month") +
+      scale_y_continuous(name = "Count",
+                         sec.axis = sec_axis(~ . / scale_factor, name = "Mean number of contacts",
+                                             breaks = contact_breaks)) +
+      labs(x = "Weeks", title = virus_name) +
+      theme_classic() +
+      theme(axis.text.x = element_text(angle = 45, hjust = 1),
+            axis.text = element_text(size = 10),
+            axis.title = element_text(size = 12),
+            legend.text = element_text(size = 11),
+            plot.title = element_text(size = 12, face = "bold"),
+            axis.title.y.right = element_text(colour = "blue"),
+            axis.text.y.right = element_text(colour = "blue"),
+            axis.line.y.right = element_line(colour = "blue"),
+            axis.ticks.y.right = element_line(colour = "blue"))
+  })
 }
 
 fig_traj <- wrap_plots(plots, ncol = 3) +
@@ -100,3 +113,40 @@ plots <- lapply(params, function(p) {
 fig_paramsbox <- wrap_plots(plots, ncol = 3) +
   plot_layout(guides = "collect") &
   theme(legend.position = "none")
+
+# plot Rt trajectories ----
+plots <- list()
+
+for (virus_name in names(results_traj)) {
+  pathogen_name <- pathogen_map[[virus_name]]
+  
+  rt_traj <- results_rt[[virus_name]]
+  
+  rt_hdi <- do.call(cbind, lapply(rt_traj, `[[`, "rt")) %>%
+    t() %>% 
+    hdi() %>%
+    rbind(mean = colMeans(do.call(cbind, lapply(rt_traj, `[[`, "rt")) %>% t())) %>%
+    t() %>% 
+    as.data.frame() %>%
+    mutate(date = rt_trajectories[[1]]$date)
+  
+  local({
+    
+    plots[[virus_name]] <<- ggplot() +
+      geom_hline(yintercept = 1, lty = 2, colour = "red") +
+      geom_ribbon(data = rt_hdi, aes(x = date, ymin = lower, ymax = upper), fill = "black", alpha = 0.2) +
+      geom_line(data = rt_hdi, aes(x = date, y = mean), colour = "black", linewidth = 1) +
+      scale_x_date(date_breaks = "3 month") +
+      scale_y_continuous(name = "R_t") +
+      labs(x = "Date", y = "R_t", title = virus_name) +
+      theme_classic() +
+      theme(axis.text.x = element_text(angle = 45, hjust = 1),
+            axis.text = element_text(size = 10),
+            axis.title = element_text(size = 12),
+            plot.title = element_text(size = 12, face = "bold"))
+  })
+}
+
+fig_traj <- wrap_plots(plots, ncol = 3) +
+  plot_layout(guides = "collect") &
+  theme(legend.position = "bottom")
