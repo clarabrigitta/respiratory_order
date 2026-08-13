a68bd9875ad4cdb4631f026be28719fd0a73d705
clarabrigitta <156320564+clarabrigitta@users.noreply.github.com>
2026-08-13 16:03:52 +0100

    add helper vector for common pathogen colours

--- R/library_and_scripts.r
+++ R/library_and_scripts.r
@@ -19,3 +19,18 @@ library(tibble)
 library(patchwork)
 library(Rcpp)
 library(coda)
+
+# helper for pathogen colours
+pathogen_cols <- c(
+  # Scottish and RCGP spellings
+  "Respiratory syncytial virus"= "#FABA39FF", "RSV"= "#FABA39FF",
+  "Influenza" = "#36AAF9FF",
+  "Influenza - Type A (any subtype)" = "#1AE4B6FF", "Influenza A" = "#1AE4B6FF",
+  "Influenza - Type B" = "#72FE5EFF", "Influenza B" = "#72FE5EFF",
+  "Rhinovirus" = "#F66B19FF", "Rhino"  = "#F66B19FF", "Entero- or Rhinovirus"= "#F66B19FF",
+  "Entero"= "#7A0403FF", 
+  "Adenovirus" = "#30123BFF", "Adeno" = "#30123BFF",
+  "Human metapneumovirus" = "#4662D7FF", "hMPV" = "#4662D7FF", "HMPV" = "#4662D7FF",
+  "Parainfluenza virus" = "#C7EF34FF", "Parainfluenza (Any Type)" = "#C7EF34FF",
+  "Seasonal coronavirus (Non-SARS-CoV-2)" = "#CB2A04FF", "OtherCoronavirus" = "#CB2A04FF", "Seasonal coronavirus" = "#CB2A04FF", "Other Coronavirus" = "#CB2A04FF"
+)
