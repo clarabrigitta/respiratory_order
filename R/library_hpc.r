fe3166c35831271a77f9aa8f13c8a73bc0dbb42b
clarabrigitta <156320564+clarabrigitta@users.noreply.github.com>
2026-08-13 10:56:40 +0100

    cut down comment

---
+++ R/library_hpc.r
@@ -0,0 +1,10 @@
+# Packages needed by the batch fit only ----
+
+library(here)
+library(dplyr)
+library(tidyr)
+library(readr)
+library(lubridate)
+library(BayesianTools)
+library(parallel)
+library(Rcpp)
